47ab63d1b470ce08673c2664c9dbdad96b0dd6ba
Ricard <ricard.castello@gmail.com>
2023-01-23 23:21:54 +0100

    chore: dependency injection at story metadata

--- src/app/heroes/Heroes.stories.ts
+++ src/app/heroes/Heroes.stories.ts
@@ -1,9 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
+import { moduleMetadata } from '@storybook/angular';
 import { Story, Meta } from '@storybook/angular/types-6-0';
 import { HeroesComponent } from './heroes.component';
 
 export default {
   title: 'TOH/Heroes',
   component: HeroesComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [HttpClientModule],
+    }),
+  ],
 } as Meta;
 
 const Template: Story<HeroesComponent> = (args: HeroesComponent) => ({
